f67e2300d7788fbee1d37ea26a03aa9ab1303d40
unknown <accounts@jadengregory.com.au>
2020-10-09 21:06:51 +1100

    Basic Game

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Sudoku from './components/Sudoku/Sudoku';
+import Sudoku from './components/Sudoku/Sudoku.jsx';
 
 function App() {
 	return (
